7e2c64d3cf36190b762e4bc80e35e3ebcbeb325c
antoniomarcuss <105389155+antoniomarcuss@users.noreply.github.com>
2024-10-04 23:36:46 -0300

    removendo erro no componente chart

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -67,9 +67,9 @@ const ChartContainer = React.forwardRef<
 ChartContainer.displayName = "Chart";
 
 const ChartStyle = ({ id, config }: { id: string; config: ChartConfig }) => {
-  const colorConfig = Object.entries(config).filter(
-    ([_, config]) => config.theme || config.color
-  );
+  const colorConfig = Object.entries(config).filter(([, config]) => {
+    return config.theme || config.color;
+  });
 
   if (!colorConfig.length) {
     return null;
